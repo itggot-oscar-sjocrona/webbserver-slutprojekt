a93eb10f694118fcea198f18df868fd7815fd6e4
itggot-oscar-sjocrona <oscar.sjocrona@itggot.se>
2018-03-29 10:41:51 +0200

    början på appen

--- app.rb
+++ app.rb
@@ -2,10 +2,39 @@ class App < Sinatra::Base
 
 	require_relative 'module.rb'
 	include Database
-
+	enable :sessions
 
 	get '/' do
-		"Hello, Grillkorv!"
+		slim(:home)
 	end
 
+	get('/error') do
+		slim(:error, locals:{error_msg:session[:error_msg], direction:session[:direction]})
+	end
+
+	post '/login' do
+		
+	end
+
+	post 'register' do
+		username = params["username"]
+		password1 = params["password1"]
+		password2 = params["password2"]
+
+		if password1 != password2
+			session[:error_msg] = "Passwords doesn't match"
+			session[:direction] = "/register"
+			redirect('/error')
+		end
+		password = BCrypt::Password.create(password1)
+		connect()
+		begin
+			#module 
+		rescue
+			session[:error_msg] = "The username has already been taken"
+			session[:direction] = "/"
+			redirect('/error')
+		end
+		redirect('/')
+	end
 end           
--- module.rb
+++ module.rb
@@ -2,4 +2,9 @@ module Database
     def connect()
         return SQLite::Database.new('/db/chatt_app.db')
     end
+
+    def register()
+        return  
+    end
+
 end
